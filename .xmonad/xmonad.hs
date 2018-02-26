b5f449819b003a6bb4c21cd89e60d2b30f09a3fa
paul <paullangton4@gmail.com>
2018-02-26 12:12:25 -0500

    now for real xmonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,20 @@
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+baseConfig = desktopConfig
+main = do
+    xmproc <- spawnPipe "xmobar"
+
+    xmonad $ baseConfig {
+                terminal = "urxvt"
+                , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+                , modMask = mod4Mask
+                }
