76a25973d3bec027047ce6b85b8c965007a58915
paul <paullangton4@gmail.com>
2018-02-27 09:45:05 -0500

    more xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,20 +11,30 @@ baseConfig = desktopConfig
 
     
 main = do
-    xmproc <- spawnPipe "xmobar"
+    xmproc <- spawnPipe "xmobar -d"
 
     xmonad $ baseConfig {
                 terminal = "urxvt"
+                , layoutHook = avoidStruts $ layoutHook baseConfig
                 , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
+                        , ppOrder               = \(ws:l:t:_)   -> [ws]
+                        , ppCurrent             = xmobarColor   colorBlue       colorGrayAlt
+                        , ppUrgent              = xmobarColor   colorGreen      colorGrayAlt
+                        , ppVisible             = xmobarColor   colorGray       colorGrayAlt
+                        , ppHidden              = xmobarColor   colorWhite      colorGrayAlt
                         }
                 , modMask = mod4Mask
+                , manageHook = manageDocks <+> manageHook baseConfig
                 }
                 `additionalKeysP` [("M-S-q", kill)
-             , ("M-<Return>", spawn "urxvt")
-             , ("M-<Space>", spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
-            ]
+                                     , ("M-<Return>", spawn "urxvt")
+                                     , ("M-<Space>", spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+                                     , ("M-<F6>", spawn "playerctl skip")
+                                     , ("M-<F5>", spawn "playerctl play-pause")
+                                     , ("M-<F4>", spawn "playerctl previous")
+                                    ]
 
                
 
